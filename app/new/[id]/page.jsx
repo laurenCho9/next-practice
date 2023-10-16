67379a480eedc3a8da723eb4730f4ad23d877f7d
laurenCho9 <lauren.choux@gmail.com>
2023-10-16 18:30:04 +0900

    Update page.jsx

--- app/new/[id]/page.jsx
+++ app/new/[id]/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NewsIdPage = ({ params }) => {
-  console.log("news id", params.id);
+  console.log("news id", params.id); // 왜 콘솔이 안보일까?
   return <div>hello {params.id}</div>;
 };
 
